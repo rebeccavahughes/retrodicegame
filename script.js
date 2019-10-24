feae36fa730e642463e97a9e9a3dee4c4d4b9b65
Code Nation <codenation@Codes-MacBook-Pro.local>
2019-10-24 11:55:42 +0100

    fixed turn issue and beautified code

--- script.js
+++ script.js
@@ -22,10 +22,10 @@ newGameFunction = () => {
     document.querySelector(".player2Info").textContent = "Player Two:";
     document.querySelector(".player1Score").textContent = (`Your Score is: ${p1Score}`)
     document.querySelector(".player2Score").textContent = (`Your Score is: ${p2Score}`)
-    
+
 }
 
-newGame.addEventListener("click", () =>{
+newGame.addEventListener("click", () => {
     newGameFunction()
 })
 
@@ -36,47 +36,47 @@ rollButton.addEventListener("click", () => {
     diceImage.src = `img/dice${randomRoll}.png`; // this appends the number of the random roll to the file
 
 
-switch (true){
-    case randomRoll != 1 && turn == "P1":
-        p1Score += randomRoll
-        turn = "P2"
-        document.querySelector(".player1Score").textContent = `Your Score is: ${p1Score}`
-        checkForWin()
-        break;
-    case randomRoll != 1 && turn == "P2":
-        p2Score += randomRoll
-        turn = "P1"
-        document.querySelector(".player2Score").textContent = `Your Score is: ${p2Score}`
-        checkForWin()
-        break;
-    default:
-        p1Score = 0;
-        p2Score = 0;
-        document.querySelector(".player1Score").textContent = "GAME OVER"
-        document.querySelector(".player2Score").textContent = "GAME OVER"
-        gameOver()
+    switch (true) {
+        case randomRoll != 1 && turn == "P1":
+            p1Score += randomRoll
+            turn = "P2"
+            document.querySelector(".player1Score").textContent = `Your Score is: ${p1Score}`
+            checkForWin()
+            break;
+        case randomRoll != 1 && turn == "P2":
+            p2Score += randomRoll
+            turn = "P1"
+            document.querySelector(".player2Score").textContent = `Your Score is: ${p2Score}`
+            checkForWin()
+            break;
+        default:
+            p1Score = 0;
+            p2Score = 0;
+            document.querySelector(".player1Score").textContent = "GAME OVER"
+            document.querySelector(".player2Score").textContent = "GAME OVER"
+            gameOver()
     }
 })
 
 const checkForWin = () => {
-    if(p1Score > 20){
+    if (p1Score > 20) {
         document.querySelector(".player1Info").textContent = "Player 1 - Winner Winner Chicken Dinner!! ";
         document.querySelector(".player2Info").textContent = "Player 2 - Loser Loser get to the boozer :(";
         gameOver()
-        
+
     }
-    if(p2Score > 20){
+    if (p2Score > 20) {
         document.querySelector(".player1Info").textContent = "Player 1 - Loser Loser get to the boozer :(";
         document.querySelector(".player2Info").textContent = "Player 2 - Winner Winner Chicken Dinner!! ";
         gameOver()
-        
+
     }
 }
 
-const gameOver = () =>{
+const gameOver = () => {
     p1Score = 0;
     p2Score = 0;
+    turn = "P1"
     rollButton.style.display = "none";
     newGame.style.display = "block"
 }
-
